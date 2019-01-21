39113f594c3d4ad70d7fcbc00d69e4446f4e4db3
Simon Green <simon.green@thielsen.co.uk>
2019-01-21 15:07:12 +0000

    Testing post vs get

--- app.rb
+++ app.rb
@@ -13,8 +13,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/named-cat-form' do
+  erb(:named_cat_form)
 end
